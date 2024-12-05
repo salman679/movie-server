5cbae4696e9f66e3ae5c38a9b9fed3259d16c02f
Salman Izhar <Sbinazhar671@gmail.com>
2024-12-05 18:38:00 +0600

    data has been inserted

---
+++ index.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+//middleware
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@salman.uweo3xy.mongodb.net/?retryWrites=true&w=majority&appName=Salman`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("MovieDB");
+    const collection = database.collection("Movies");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
