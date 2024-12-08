e30c847ac47e744e1d2585d7c2d5ef3919dc982d
Salman Izhar <Sbinazhar671@gmail.com>
2024-12-09 01:31:16 +0600

    updated in add-movie api

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@salman.uweo3xy.mongodb.net/?retryWrites=true&w=majority&appName=Salman`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,10 +33,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const database = client.db("MovieDB");
     const movieCollection = database.collection("Movies");
+    const favoriteCollection = database.collection("Favorites");
 
     app.get("/movies", async (req, res) => {
       const result = await movieCollection
@@ -45,6 +46,11 @@ async function run() {
         .limit(6)
         .toArray();
 
+      res.send(result);
+    });
+
+    app.get("/all-movies", async (req, res) => {
+      const result = await movieCollection.find().toArray();
       res.send(result);
     });
 
@@ -58,9 +64,6 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await movieCollection.findOne(query);
-
-      console.log(result);
-
       res.send(result);
     });
 
@@ -93,6 +96,59 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await movieCollection.deleteOne(query);
       res.send(result);
+    });
+
+    //favorite collection
+    app.post("/add-favorite", async (req, res) => {
+      const { userEmail, movieId } = req.body;
+
+      const existingFavorite = await favoriteCollection.findOne({
+        userEmail,
+        movieId,
+      });
+
+      if (existingFavorite) {
+        return res.status(400).send("Movie already in favorites.");
+      }
+
+      const result = await favoriteCollection.insertOne({
+        userEmail,
+        movieId,
+      });
+
+      res.json(result);
+    });
+
+    app.get("/favorites/:email", async (req, res) => {
+      const userEmail = req.params.email;
+
+      const favoriteMovies = await favoriteCollection
+        .find({ userEmail })
+        .toArray();
+
+      const movieIds = favoriteMovies.map(
+        (movie) => new ObjectId(movie.movieId)
+      );
+
+      const movies = await movieCollection
+        .find({ _id: { $in: movieIds } })
+        .toArray();
+
+      res.json(movies);
+    });
+
+    app.delete("/favorites/:email/:movieId", async (req, res) => {
+      const userEmail = req.params.email;
+      const movieId = req.params.movieId;
+
+      console.log(req.params);
+
+      const result = await favoriteCollection.deleteOne({
+        userEmail,
+        movieId,
+      });
+
+      res.json(result);
     });
 
     // Send a ping to confirm a successful connection
