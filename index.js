14cb600fec94a5e6454f8aef1a5ceb87f9eaf741
Salman Izhar <Sbinazhar671@gmail.com>
2024-12-09 06:21:45 +0600

    fix the api error

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const cors = require("cors");
 
 require("dotenv").config();
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 //middleware
 app.use(express.json());
